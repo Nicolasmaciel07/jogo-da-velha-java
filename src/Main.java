81eda1cad672a5bd1a4e5490e3cbeb13d97a16bb
Nicolasmaciel07 <nicolas.maciel.batista20@gmail.com>
2026-08-17 21:16:25 -0300

    Primeiro commit - jogo da velha

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,6 @@ public class Main {
         int turno = 1;
         boolean jogoRodando = true;
         int jogadasTotais = 0;
-
         while (jogoRodando) {
             char jogadorAtual;
 
@@ -29,23 +28,28 @@ public class Main {
                 System.out.println();
                 if (i < 2) System.out.println("-----------");
             }
+
             System.out.println("É a vez do Jogador " + turno + " (" + jogadorAtual + ")");
-            System.out.println("Informe a linha (0, 1 ou 2):");
-            int linha = scanner.nextInt();
-            System.out.println("Informe a coluna (0, 1 ou 2):");
-            int coluna = scanner.nextInt();
+            System.out.println("Informe a linha (1, 2 ou 3):");
+            int linha = scanner.nextInt() - 1;
+
+            System.out.println("Informe a coluna (1, 2 ou 3):");
+            int coluna = scanner.nextInt() - 1;
 
             if (linha < 0 || linha > 2 || coluna < 0 || coluna > 2 || tabuleiro[linha][coluna] != ' ') {
                 System.out.println("Posição inválida.");
                 continue;
             }
+
             tabuleiro[linha][coluna] = jogadorAtual;
             jogadasTotais++;
             boolean venceu = false;
+
             for (int i = 0; i < 3; i++) {
                 if (tabuleiro[i][0] == jogadorAtual && tabuleiro[i][1] == jogadorAtual && tabuleiro[i][2] == jogadorAtual) venceu = true;
                 if (tabuleiro[0][i] == jogadorAtual && tabuleiro[1][i] == jogadorAtual && tabuleiro[2][i] == jogadorAtual) venceu = true;
             }
+
             if (tabuleiro[0][0] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][2] == jogadorAtual) venceu = true;
             if (tabuleiro[0][2] == jogadorAtual && tabuleiro[1][1] == jogadorAtual && tabuleiro[2][0] == jogadorAtual) venceu = true;
 
@@ -60,6 +64,7 @@ public class Main {
                 jogoRodando = false;
                 break;
             }
+
             if (turno == 1) {
                 turno = 2;
             } else {
